634e94de14324a7283b39ecaa8b9a37fc058cbed
omar <odzinic@gmail.com>
2021-03-11 15:05:47 -0500

    - Rows can be added via button

--- src/js/client.js
+++ src/js/client.js
@@ -1,13 +1,23 @@
+let tblIngredients = document.getElementById("tblEmpAllowances").getElementsByTagName('tbody')[0];
+document.getElementById("menu1").style.visibility = "hidden";
+
+
+function ingredRow(){
+    let newRow = tblIngredients.insertRow(-1);
+    let newCell = newRow.insertCell(0);
+    newCell.innerText = "Hi";
+}
+
 $(document).ready(function () {
 
     // Query ingredient api to retrieve list of ingredients in database and populate dropdown menu
     $.ajax("/api/ingredients/")
         .done(function (data){
             for (let i=0;i<data.length;i++){
-                console.log(data[i]);
                 $('#dropdownValues').append(`<a class="dropdown-item" href="#">${data[i].items}</a>`);
             }
+            $('#btnSave').on("click", ingredRow);
 
         })
-    
+
 });
