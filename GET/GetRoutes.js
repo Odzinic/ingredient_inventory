864ca91a1565596ba6efbde9012befa2802a2988
omar <odzinic@gmail.com>
2021-03-14 19:32:40 -0400

    - Added API for retrieving nutrient data - Rows populated by nutrient data - Added input forms

--- GET/GetRoutes.js
+++ GET/GetRoutes.js
@@ -18,4 +18,26 @@ router.get('/api/ingredients/', async(req, res) => {
     }
 })
 
+// Retrieve ingredient values
+router.get('/api/values/', async(req, res) => {
+
+    if (Object.entries(req.query).length == 0){
+        res.status(500)
+    }
+    else{
+        let ingredReq = req.query.ingred;
+
+        let statement = `SELECT fat_cont, fibre_cont, sugr_cont, prot_cont, carb_cont FROM ingredient_db.Ingredients WHERE ingred_name = "${ingredReq}"`;
+
+        try {
+            let pool = await dbPool;
+            let result = await pool.query(statement);
+            res.status(200).send(result);
+        } catch (err) {
+            res.status(500).send(err);
+        }
+    }
+
+})
+
 module.exports = router;
--- src/js/client.js
+++ src/js/client.js
@@ -1,23 +1,64 @@
 let tblIngredients = document.getElementById("tblEmpAllowances").getElementsByTagName('tbody')[0];
-document.getElementById("menu1").style.visibility = "hidden";
+let ingredLst="";
+let idVal =  0;
 
+function updateAmnt(row){
+}
+
+function updateCont(row, drop, ingred){
+    $.ajax(`/api/values?ingred=${ingred}`)
+        .done(function (data){
+            console.log(data);
+            drop.text(`${ingred}`);
+            //TODO: Makes these two cells change
+            row.cells[2].innerText = "100";
+            row.cells[3].innerText = "0";
+
+            row.cells[4].innerText = data[0]["fat_cont"];
+            row.cells[5].innerText = data[0]["fibre_cont"];
+            row.cells[6].innerText = data[0]["sugr_cont"];
+            row.cells[7].innerText = data[0]["prot_cont"];
+            row.cells[8].innerText = data[0]["carb_cont"];
+
+    });
+
+
+
+}
 
 function ingredRow(){
+    idVal++;
     let newRow = tblIngredients.insertRow(-1);
-    let newCell = newRow.insertCell(0);
-    newCell.innerText = "Hi";
+    let dropdownCell = newRow.insertCell(0);
+    let amntCell = newRow.insertCell(1);
+    let perCell = newRow.insertCell(2);
+    let costCell = newRow.insertCell(3);
+    let fatCell = newRow.insertCell(4);
+    let fibreCell = newRow.insertCell(5);
+    let sugCell = newRow.insertCell(6);
+    let protCell = newRow.insertCell(7);
+    let carbCell = newRow.insertCell(8);
+
+    $(dropdownCell).append(`<button class="btn btn dropdown-toggle" type="button" id="dropdownMenuButton_${idVal}" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Select Ingredients</button><ul class="dropdown-menu" id="dropdown_${idVal}" aria-labelledby="dropdownMenuButton"">${ingredLst}</ul>`);
+    $(amntCell).append(`<div><input type="number" class="form-control" id="exampleInputEmail1" placeholder="Enter amount"></div>`);
+
+    let dropMenu = document.getElementById(`dropdown_${idVal}`);
+
+    // $(`#dropdown_${idVal} li`).on('click', function(){console.log($(this).text())});
+    $(`#dropdown_${idVal} li`).on('click', function(){updateCont(newRow, $(`#dropdownMenuButton_${idVal}`), $(this).text())});
+    amntCell.addEventListener('input', function(){updateAmnt(newRow)});
+
 }
 
 $(document).ready(function () {
-
-    // Query ingredient api to retrieve list of ingredients in database and populate dropdown menu
     $.ajax("/api/ingredients/")
-        .done(function (data){
-            for (let i=0;i<data.length;i++){
-                $('#dropdownValues').append(`<a class="dropdown-item" href="#">${data[i].items}</a>`);
+        .done(function (data) {
+            for (let i = 0; i < data.length; i++) {
+                ingredLst += `<li><a class="dropdown-item" href="#">${data[i].items}</a></li>`;
             }
-            $('#btnSave').on("click", ingredRow);
-
+            ingredRow()
         })
 
-});
+    $('#btnSave').on("click", ingredRow);
+
+})
